84e3267fdededee86f02bbb01bbfb23045ac956b
DYLAN WERE <102102160@student.swin.edu.au>
2022-09-27 18:03:04 +1000

    GUI Index implementation

--- MarkerDetection/GUI.py
+++ MarkerDetection/GUI.py
@@ -15,11 +15,33 @@ class Gui:
     def home(self):
         self.state = "home"
         window = tk.Tk()
-        greeting = tk.Label(
-            text="Choose an option",
-            background="dark slate grey"
-        )
-        greeting.pack()
+        index()
+
+        
+        
+        
+            
+
+
+    def index(self):
+        for i in range(4):
+            frame = tk.Frame(
+                master=window,
+                relief=tk.RAISED,
+                borderwidth=1)
+            frame.grid(row=1, column=i)
+            label = tk.Label(master=frame, text=f"Row {1}\nColumn {i}")
+            if i==0:
+                label = tk.Label(master=frame, text="Home", )
+            if i==1:
+                label = tk.Label(master=frame, text="Generate Tags")
+            if i==2:
+                label = tk.Label(master=frame, text="Calibrate Camera")
+            if i==3:
+                label = tk.Label(master=frame, text="Detect Pose")
+            label.pack()
+
+
         window.mainloop()
 
 
